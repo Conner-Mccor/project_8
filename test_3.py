4f76eedb3a8226348be703882cd5e5837544245d
Conner-Mccor <connermccor@gmail.com>
2025-11-15 20:02:27 -0500

    Not sure what happened, here....

--- test_3.py
+++ test_3.py
@@ -1,20 +1,3 @@
-# main()
-name = input("What is your name?")
-print(f"Hello {name}.")
-hex = 0
-decimal = 0
-if var.startswith("0x"):
-    print("This is a hex.")
-    hex = 1
-else:
-    print("This is a decimal")
-    decimal = 1
-if hex == 1:
-    binary_value = bin(int(var, 16))
-    print(f"{binary_value}")
-if decimal == 1:
-    binary_value = bin(int(var, 10))
-    print(f"{binary_value}")
 def main():
     name = input("What is your name?")
     print(f"Hello {name}.")
